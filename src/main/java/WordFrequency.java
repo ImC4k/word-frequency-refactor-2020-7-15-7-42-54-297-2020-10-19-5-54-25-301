f21f6aa442b49249dfac47c8056966a1f178410a
Calvin Chu <calvinchu26@gmail.com>
2020-11-30 17:34:40 +0800

    [refactor][Calvin] refactor: rename meaningless variables

--- src/main/java/WordFrequency.java
+++ src/main/java/WordFrequency.java
@@ -2,9 +2,9 @@ public class WordFrequency {
     private final String word;
     private final int frequency;
 
-    public WordFrequency(String w, int i) {
-        this.word = w;
-        this.frequency = i;
+    public WordFrequency(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
     }
 
 
